92658d6f412476d7c5d4925dc219ec9b9075725f
Godwin <godwinaquinas@gmail.com>
2024-12-02 21:02:10 +0100

    implement zora contract call

--- src/app/.well-known/frames.json/route.ts
+++ src/app/.well-known/frames.json/route.ts
@@ -3,7 +3,7 @@ export async function GET() {
 
   const config = {
     config: {
-      version: "0.1.0",
+      version: "0.0.0",
       name: "Zora Launcher",
       icon: `${appUrl}/icon.png`,
       splashImage: `${appUrl}/splash.png`,
